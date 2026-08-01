1a0c0f3d480e5967fc2e892fe830e19e0e703555
Ubeyd Özdemir <SENIN-GITHUB-EMAILIN@ornek.com>
2026-08-01 19:19:33 +0300

    Takvim bileşenleri Türkçeleştirildi (LocaleConfig, kök layout)

--- babi-app/src/app/_layout.tsx
+++ babi-app/src/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from 'expo-router';
+import { LocaleConfig } from 'react-native-calendars';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+LocaleConfig.locales['tr'] = {
+  monthNames: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+  monthNamesShort: ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+  dayNames: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+  dayNamesShort: ['Paz', 'Pzt', 'Sal', 'Çar', 'Per', 'Cum', 'Cmt'],
+  today: 'Bugün',
+};
+LocaleConfig.defaultLocale = 'tr';
 
 export default function RootLayout() {
   return (
